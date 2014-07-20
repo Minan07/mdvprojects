68afeba45fe6b7a31314ffdd6ca9596627de0619
Minan07 <minanematalla@Minas-MacBook-Pro.local>
2014-07-19 22:42:00 -0400

    SDI Lab 6

--- sdi-lab6/nematalla_mina_lab6.js
+++ sdi-lab6/nematalla_mina_lab6.js
@@ -1,15 +1,41 @@
 //alert("JavaScript works!");
 
+// Mina G. Nematalla
+// SDI 1407
+// Lab 6 
+// July 19, 2014
+
+
+
 var sportsTeam = function(){
 	
 	sportsTeam = ["Heat", "Cavaliers", "Thunder", "Magic"];
 	city = ["Miami", "Cleveland","Oklahoma", "Orlando"];
 
+	var sportsTeam = ["Heat", "Cavaliers", "Thunder", "Magic", "Spurs"];
+	console.log(sportsTeam);
+	
+	
 	for (i = 0; i < city.length; i++) {
 	
 		console.log(city[i] + " is the city that the " + sportsTeam[i]+ " is located in.");	
 	
 	}	
+
+	var sportsTeam = ["Heat", "Cavaliers", "Thunder", "Magic", "Spurs"];
+	console.log(sportsTeam);
+	
+	
+	var nbaTeam = sportsTeam.push();
+	sportsTeam.push("Rockets", "Spurs");
+	console.log(sportsTeam);
+	
+	var nbaTeam = sportsTeam.pop();
+	sportsTeam.pop("Rockets");
+	console.log(sportsTeam);
+	
+	
+	
 };
 
 sportsTeam();
